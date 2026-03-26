c6148589512d63766ba1b98d5892e0904b9e13ce
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-03-26 18:10:58 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ export async function onRequest(context) {
 
   // 판매 페이지 주소 세팅
   const kmongUrl = "https://kmong.com/gig/대표님크몽주소";
-  const fiverrUrl = "https://fiverr.com/대표님파이버주소";
+  const fiverrUrl = "https://www.fiverr.com/tonycho999/build-a-custom-qr-code-data-tracking-dashboard-in-google-sheets";
 
   // 접속 국가가 한국(KR)이면 크몽으로, 그 외의 모든 국가는 파이버로 리다이렉트
   if (country === "KR") {
